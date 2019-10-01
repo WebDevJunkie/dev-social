6269bbe25cdb11fbb5f45e1d561c1af18fdd3fce
Mike Wittenauer <mikewittenauer@me.com>
2019-10-01 15:40:20 -0400

    Create add post end point

--- api/routes/api/posts.js
+++ api/routes/api/posts.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route  GET api/posts
-// @desc   Test route
-// @access Public
-router.get('/', (req, res) => res.send('Posts route'));
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post('/', [ auth, [
+    check('text', 'Text is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar
+        });
+
+        await newPost.save();
+        res.json(newPost);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 module.exports = router;
